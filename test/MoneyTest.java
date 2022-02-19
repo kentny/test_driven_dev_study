1ab9ece0ef78c70d41307e5c406c38bbe6d26acc
kentny <kentaro.terasaki@gmail.com>
2022-02-19 11:15:06 +0900

    Add equality.

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -1,12 +1,21 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
     public void testMoneyMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(five.amount, 10);
+        Dollar product = five.times(2);
+        assertEquals(product.amount, 10);
+
+        product = five.times(3);
+        assertEquals(product.amount, 15);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
